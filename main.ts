95ff36d25a1b83062ad612609e4d72a5622f7ade
Cheng Jen Li <neo@cpshs.org.tw>
2022-03-11 09:26:36 +0800

    廣播版的猜拳

--- main.ts
+++ main.ts
@@ -1,11 +1,20 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    對手出拳 = receivedNumber
+    while (我出拳 != 0) {
+        比輸贏()
+        basic.pause(3000)
+        設定初值()
+    }
+})
 input.onButtonPressed(Button.A, function () {
     我出拳 = 1
     basic.showIcon(IconNames.Scissors)
+    radio.sendNumber(我出拳)
 })
 function 設定初值 () {
     basic.showArrow(ArrowNames.North)
     我出拳 = 0
-    對手出拳 = randint(1, 3)
+    對手出拳 = 0
 }
 function 比輸贏 () {
     if (我出拳 == 對手出拳) {
@@ -23,19 +32,14 @@ function 比輸贏 () {
 input.onButtonPressed(Button.AB, function () {
     我出拳 = 3
     basic.showIcon(IconNames.Square)
+    radio.sendNumber(我出拳)
 })
 input.onButtonPressed(Button.B, function () {
     我出拳 = 2
     basic.showIcon(IconNames.SmallDiamond)
+    radio.sendNumber(我出拳)
 })
-let 對手出拳 = 0
 let 我出拳 = 0
+let 對手出拳 = 0
+radio.setGroup(84)
 設定初值()
-basic.forever(function () {
-    serial.writeNumber(對手出拳)
-    while (我出拳 != 0) {
-        比輸贏()
-        basic.pause(3000)
-        設定初值()
-    }
-})
